271bff9411462f0e08882cadb262eb9664c60e41
Flash-kir <flash-kir@yandex.ru>
2023-03-13 17:06:40 +0300

    static files

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -105,11 +105,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_files'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
