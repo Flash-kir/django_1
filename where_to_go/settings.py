ba4a42c9183072b67136cf0ae50f8721aca6b2c4
Flash-kir <flash-kir@yandex.ru>
2023-03-13 12:51:08 +0300

    add *.pythonanywhere.com to hosts

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -108,6 +108,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = '/static_files/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
