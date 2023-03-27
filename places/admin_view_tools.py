c74e1c706693b6d0974689b7dcbb986052a3037c
Flash-kir <flash-kir@yandex.ru>
2023-03-28 00:04:31 +0300

    delete useless function

--- places/admin_view_tools.py
+++ places/admin_view_tools.py
@@ -1,13 +1,9 @@
 from django.utils.html import format_html
 
 
-def image_html_format(url, height=200):
+def preview_image(self, height=200):
     return format_html(
-            '<img src="{}" height={} />',
-            url,
-            height,
-        )
-
-
-def preview_image(self):
-    return image_html_format(self.image.url)
+        '<img src="{}" height={} />',
+        self.image.url,
+        height,
+    )
