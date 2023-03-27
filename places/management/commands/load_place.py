c0c2328474fa3b5360730d9d72a95e8aaf706309
Flash-kir <flash-kir@yandex.ru>
2023-03-27 10:05:07 +0300

    use .get() method of dict, change errors handling

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -34,21 +34,20 @@ class Command(BaseCommand):
         try:
             response = requests.get(url)
             response.raise_for_status()
-            place_content = []
-            response.json()
+            place_content = response.json()
 
-            title = place_content['title']
-            lat = place_content['coordinates']['lat']
-            lng = place_content['coordinates']['lng']
-            description_short = place_content['description_short']
-            description_long = place_content['description_long']
+            title = place_content.get('title')
+            if not title:
+                raise Exception('File do not contain place title')
 
+            if place_content.get('coordinates'):
+                lat = place_content['coordinates'].get('lat')
+                lng = place_content['coordinates'].get('lng')
             if not lat or not lng:
                 raise Exception('File do not contain place coordinates')
-            if not description_short:
-                description_short = ''
-            if not description_long:
-                description_long = ''
+
+            description_short = place_content.get('description_short', '')
+            description_long = place_content.get('description_long', '')
 
             place, created = Place.objects.get_or_create(
                 title=title,
@@ -59,7 +58,7 @@ class Command(BaseCommand):
                 place.description_short = description_short
                 place.description_long = description_long
                 place.save()
-                for position, image_path in enumerate(place_content['imgs']):
+                for position, image_path in enumerate(place_content.get('imgs')):
                     load_image(place, image_path, position)
             else:
                 print(f'place {place_content["title"]} allready exist')
